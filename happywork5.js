6b956926200467c0a5b5890a5f9be63ba114b481
mutify <147934542+mutaz13@users.noreply.github.com>
2023-10-14 18:32:27 +0300

    Add files via upload

---
+++ happywork5.js
@@ -0,0 +1,17 @@
+var arrays =  myfunction[1,2,3,4,5]
+
+function myfunction(arrays ){
+    var arrays=[];
+    for (var i = 0; i<5 ; i++)
+    if (arrays[i] > 10)
+    arrays.push
+     return arrays[i]
+
+     
+    
+
+}
+
+
+
+   
